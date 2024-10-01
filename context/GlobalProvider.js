e2a3e5581a53466fc339ce0c4d8afcdb61689b74
Rida702 <ai22c0rida@gmail.com>
2024-10-01 15:16:38 +0500

    first commit

---
+++ context/GlobalProvider.js
@@ -0,0 +1,49 @@
+//Now to make the context API avaiable to all the screens we will wrap the 
+//main layout.jsx with it so that it is accessible from all the screens
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+import { getCurrentUser } from "../lib/appwrite";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getCurrentUser()
+      .then((res) => {
+        if (res) {
+          setIsLogged(true);
+          setUser(res);
+        } else {
+          setIsLogged(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLogged,
+        setIsLogged,
+        user,
+        setUser,
+        loading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
